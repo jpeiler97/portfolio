190bd8e81ca4c6d0a60904afb7a9a3b876b0fd05
JP Eiler <jp.eiler97@gmail.com>
2021-06-25 11:09:54 -0500

    can cycle through projects using buttons

--- src/components/pages/ProjectPage.js
+++ src/components/pages/ProjectPage.js
@@ -3,25 +3,51 @@ import Project from '../Project';
 import projects from '../../projects.json';
 
 function ProjectPage() {
-	const [ projectState, setProjectState ] = useState({
-		projectList: projects,
-		projectIndex: 0,
-		title: projects[0].title,
-		gitLink: projects[0].gitLink,
-		projectLink: projects[0].projectLink,
-		image: projects[0].image,
-		imageAlt: projects[0].imageAlt
+	const [ state, setProjectState ] = useState({
+		projectIndex: 0
 	});
+
+	function changeIndex(event) {
+		event.preventDefault();
+		console.log(projects.length);
+		if (state.projectIndex <= projects.length - 1 && state.projectIndex >= 0) {
+			console.log('something');
+			if (event.target.innerText === 'Previous' && state.projectIndex - 1 !== -1) {
+				setProjectState({
+					...state,
+					projectIndex: state.projectIndex - 1
+				});
+			} else if (event.target.innerText === 'Next' && state.projectIndex + 1 !== projects.length) {
+				setProjectState({
+					...state,
+					projectIndex: state.projectIndex + 1
+				});
+			} else if (event.target.innerText === 'Previous' && state.projectIndex - 1 === -1) {
+				setProjectState({
+					...state,
+					projectIndex: 5
+				});
+			} else if (event.target.innerText === 'Next' && state.projectIndex + 1 === projects.length) {
+				setProjectState({
+					...state,
+					projectIndex: 0
+				});
+			}
+		}
+	}
 
 	return (
 		<section className="app-section" id="app-section">
 			<div id="section-title">What have I made?</div>
+			{state.projectIndex + 1} of 6
+			<button onClick={changeIndex}>Next</button>
+			<button onClick={changeIndex}>Previous</button>
 			<Project
-				title={projectState.title}
-				gitLink={projectState.gitLink}
-				projectLink={projectState.projectLink}
-				image={projectState.image}
-				imageAlt={projectState.imageAlt}
+				title={projects[state.projectIndex].title}
+				gitLink={projects[state.projectIndex].gitLink}
+				projectLink={projects[state.projectIndex].projectLink}
+				image={projects[state.projectIndex].image}
+				imageAlt={projects[state.projectIndex].imageAlt}
 			/>
 		</section>
 	);
