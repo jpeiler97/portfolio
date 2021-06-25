271267b03b22706845e356c62cc706b51270efd7
JP Eiler <jp.eiler97@gmail.com>
2021-06-25 14:03:52 -0500

    added styling for the project area and footer

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,30 +2,30 @@ import React from 'react';
 
 function Project({ title, gitLink, projectLink, image, imageAlt }) {
 	return (
-		// <div className="app-card">
-		// 	<a href={projectLink} target="_blank">
-		// 		<img src={image} href={imageAlt} />
-		// 	</a>
-		// 	<div id="app-tag">{title}</div>
-		// 	<div id="repo-link">
-		// 		<a href={gitLink} target="_blank">
-		// 			Github Repo
-		// 		</a>
-		// 	</div>
-		// </div>
 		<div className="app-area">
 			<div className="app-title">{title}</div>
+			<hr />
 			<div className="app-info">
-				<div className="repo">
-					<a href={gitLink} target="_blank">
-						Github Repo
-					</a>
+				<div className="app-links">
+					<div className="repo">
+						<a href={gitLink} target="_blank">
+							Github Repo
+						</a>
+					</div>
+					<div className="deploy">
+						<a href={projectLink} target="_blank">
+							Deployed App
+						</a>
+					</div>
+					<hr />
+					<div className="description">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi scelerisque, magna eget rutrum
+						laoreet, libero nisl tincidunt eros, sit amet porttitor purus justo in ex. Fusce feugiat at erat
+						quis aliquet. Integer interdum nunc et est malesuada iaculis. Sed turpis sem, interdum non
+						placerat quis, ornare a sem.
+					</div>
 				</div>
-				<div className="deploy">
-					<a href={projectLink} target="_blank">
-						Deployed App
-					</a>
-				</div>
+
 				<img src={image} href={imageAlt} />
 			</div>
 		</div>
